dfe41cf2f581be8586c05ac54233521a86d3a24a
orderil <orderil@mail.ru>
2020-02-10 22:15:17 -0500

    changed a bit

--- src/main/kotlin/com/ilazarev/kotlin/quickstart/Person.kt
+++ src/main/kotlin/com/ilazarev/kotlin/quickstart/Person.kt
@@ -1,9 +1,17 @@
 package com.ilazarev.kotlin.quickstart
 
-class Person(val name: String, val isMarried: Boolean)
+class Person(val name: String, val married: Boolean)
 
 fun main(args: Array<String>) {
-    val person = Person("Igor", true)
-    println("Hello, ${person.name} !");
-    println("${person.isMarried}");
+    show(Person("Igor", true))
+    show(Person("John", false))
+}
+
+private fun show(person: Person) {
+    print("Hello, ${person.name}! ");
+    var word = "";
+    if (!person.married) {
+        word = "not "
+    }
+    println("You are " + word + "married.");
 }
